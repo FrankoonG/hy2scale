0eb7d64230616a93cd09e1f55ecb3f4f441b85a1
FrankoonG <FrankoonG@users.noreply.github.com>
2026-03-24 22:54:15 +1100

    v1.1.1

--- internal/api/server.go
+++ internal/api/server.go
@@ -412,7 +412,7 @@ func (s *Server) getStats(w http.ResponseWriter, r *http.Request) {
 }
 
 // Version is the application version. Update this on each release.
-const Version = "1.1.0"
+const Version = "1.1.1"
 
 func (s *Server) getNode(w http.ResponseWriter, r *http.Request) {
 	cfg := s.app.Store().Get()
