3477db7ba9bc58d71e1d49173c0b30ee98d7dd7e
FrankoonG <FrankoonG@users.noreply.github.com>
2026-05-17 19:15:40 +1000

    chore: bump version to 1.3.8

--- internal/api/server.go
+++ internal/api/server.go
@@ -946,7 +946,7 @@ func (s *Server) getStats(w http.ResponseWriter, r *http.Request) {
 }
 
 // Version is the application version. Update this on each release.
-const Version = "1.3.7"
+const Version = "1.3.8"
 
 func init() {
 	app.AppVersion = Version
